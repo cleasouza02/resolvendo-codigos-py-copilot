9804b3dc0821193fdc43af9e61ff61c58a0ac810
Clea Souza <clea.vlima22@gmail.com>
2025-05-15 18:04:23 +0000

    Resolução dos exercicios propostos pela DIO

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,4 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+string= input("Digite uma string: ")
+repeticoes = int(input("Digite o número de repetições: "))
+print((string + " " )* repeticoes)
